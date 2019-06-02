ad7ca2c179a56d435b94c70fd965bbb6ce527ad7
Sridhar Ratnakumar <srid@srid.ca>
2019-06-02 12:51:39 -0400

    Put overrides in separate module

--- core/default.nix
+++ core/default.nix
@@ -5,33 +5,9 @@ let
 in
 compiler.developPackage {
   root = ./.;
-  source-overrides = {
-    aeson-gadt-th = pkgs.fetchFromGitHub {
-      owner = "obsidiansystems";
-      repo = "aeson-gadt-th";
-      rev = "e40c293901a9cb9be4b0748109f4bc6806bfdb79";
-      sha256 = "08iqyzd4240g7af2lwgxmqbfglyxxii43i2zi01xmk1kg8inzs3v";
-    };
-    constraints-extras = pkgs.fetchFromGitHub {
-      owner = "obsidiansystems";
-      repo = "constraints-extras";
-      rev = "f6a12f9403c77ae075029171fc572e7d26222bb7";
-      sha256 = "1kflqgn5zcmd5f7z8m4z45xd0ybgh080qi9x9dkl15qsszrca6ry";
-    };
-    dependent-sum-aeson-orphans = pkgs.fetchFromGitHub {
-      owner = "obsidiansystems";
-      repo = "dependent-sum-aeson-orphans";
-      rev = "75d7f07b5ac357fb20494f98f6d865b78ee44b06";
-      sha256 = "1wx4l1mgyplcpczj7n79vf3p0n1fbbfzvh17nw3cqf4rscvp8y0f";
-    };
-  };
+  source-overrides = import ../overrides.nix { inherit pkgs; };
 
   overrides = self: super: with pkgs.haskell.lib; {
-    # 0.4 version is not released yet.
-    # TODO: upgrade to latest.
-    algebraic-graphs = dontCheck (self.callCabal2nix "algebraic-graphs" (builtins.fetchTarball {
-      url = "https://github.com/snowleopard/alga/archive/1754312.tar.gz";
-      sha256 = "09gp4vgslcr9r5w8h5jm1la0c5fnzxmhiv530rq7i16sisfrj4rr";
-    }) {});
+    algebraic-graphs = dontCheck super.algebraic-graphs;
   };
 }
---
+++ overrides.nix
@@ -0,0 +1,28 @@
+{ pkgs }: {
+  aeson-gadt-th = pkgs.fetchFromGitHub {
+    owner = "obsidiansystems";
+    repo = "aeson-gadt-th";
+    rev = "e40c293901a9cb9be4b0748109f4bc6806bfdb79";
+    sha256 = "08iqyzd4240g7af2lwgxmqbfglyxxii43i2zi01xmk1kg8inzs3v";
+  };
+  constraints-extras = pkgs.fetchFromGitHub {
+    owner = "obsidiansystems";
+    repo = "constraints-extras";
+    rev = "f6a12f9403c77ae075029171fc572e7d26222bb7";
+    sha256 = "1kflqgn5zcmd5f7z8m4z45xd0ybgh080qi9x9dkl15qsszrca6ry";
+  };
+  dependent-sum-aeson-orphans = pkgs.fetchFromGitHub {
+    owner = "obsidiansystems";
+    repo = "dependent-sum-aeson-orphans";
+    rev = "75d7f07b5ac357fb20494f98f6d865b78ee44b06";
+    sha256 = "1wx4l1mgyplcpczj7n79vf3p0n1fbbfzvh17nw3cqf4rscvp8y0f";
+  };
+  # 0.4 version is not released yet.
+  # TODO: upgrade to latest.
+  algebraic-graphs = pkgs.fetchFromGitHub {
+    owner = "snowleopard";
+    repo = "alga";
+    rev = "1754312";
+    sha256 = "09gp4vgslcr9r5w8h5jm1la0c5fnzxmhiv530rq7i16sisfrj4rr";
+  };
+}
